8d3195dcffabe8c1249239189cbcfe88dfaa4d0a
ArthurDrc <44248355+ArthurDrc@users.noreply.github.com>
2019-01-04 20:49:36 +0800

    Update initialize_nginx_conf_debian.sh

--- initialize_nginx_conf_debian.sh
+++ initialize_nginx_conf_debian.sh
@@ -3,12 +3,12 @@ read name
 sudo touch /etc/nginx/sites-enabled/$name
 echo "Enter the domain or IP your want your conf use"
 read ip
-echo "Enter the html root dir you want your conf use (usually /var/www/your_domain_name"
+echo "Enter the html root dir you want your conf use (usually /var/www/your_domain_name)"
 read dir
 sudo echo "server {
     listen 80;
 
-    root /var/www/$dir;
+    root $dir;
     index index.nginx-debian.html index.php index.html index.htm;
 
     server_name $ip;
